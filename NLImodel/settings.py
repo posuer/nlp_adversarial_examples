0695ddcb94aaaf7918441f043b47946f58c6a03b
haejupark <phj0225@yonsei.ac.kr>
2019-01-15 18:12:04 +0900

    Update settings.py

--- NLImodel/settings.py
+++ NLImodel/settings.py
@@ -10,10 +10,10 @@ class Setting(object):
 		self.keep_prob = 0.6
 		self.batch_size = 1024
 		self.epochs = 50
-    # your directory here
-		self.train_dir = "data/xnli-mt/multinli/"
-		self.dev_dir = "data/xnli/"
-		self.embed_dir = "../aaai2019/data/"
+    		# your directory here
+		self.train_dir = ""
+		self.dev_dir = ""
+		self.embed_dir = ""
 
 labels = {'neutral':0, 'entailment':1, 'contradiction':2}
 labels_reverse = {0:'neutral', 1:'entailment', 2:'contradiction'}
