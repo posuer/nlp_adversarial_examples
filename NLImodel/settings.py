70b0555ccab262ec308b036345b83ba3658e0f1a
Wang Gengyu <posuer000@gmail.com>
2019-01-17 12:03:41 +0900

    add textual entailment model (#1)
    * Create models.py
    
    * Update models.py
    
    * Update models.py
    
    * Create settings
    
    * Rename settings to settings.py
    
    * Create data_loader.py
    
    * Update data_loader.py
    
    * Update settings.py

---
+++ NLImodel/settings.py
@@ -0,0 +1,20 @@
+import sys
+
+class Setting(object):
+	def __init__(self):
+		self.lr = 0.01
+		self.word_dim = 300
+		self.lstm_dim = 256
+		self.max_len = 30
+		self.dense_dim =  128
+		self.keep_prob = 0.6
+		self.batch_size = 1024
+		self.epochs = 50
+    		# your directory here
+		self.train_dir = ""
+		self.dev_dir = ""
+		self.embed_dir = ""
+
+labels = {'neutral':0, 'entailment':1, 'contradiction':2}
+labels_reverse = {0:'neutral', 1:'entailment', 2:'contradiction'}
+
